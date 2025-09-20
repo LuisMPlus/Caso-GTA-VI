6b1f428df8074672aa735e50bafd2a67c3de988b
LuisMPlus <lx147812345@gmail.com>
2025-09-20 20:23:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "tailwindcss";
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
